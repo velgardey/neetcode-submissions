1ad86ae620d40b0b8aa8e64904f95c43f3ce6137
Mriganka Dey <arch.velgardey@gmail.com>
2026-05-13 15:36:19 +0530

    Add: permutations - submission-7

---
+++ Data Structures & Algorithms/permutations/submission-7.py
@@ -0,0 +1,27 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        
+        # Nested helper function captures 'res' and 'nums' from the outer scope
+        def backtrack(perm, pick):
+            # Base case: if the current permutation is the right length
+            if len(perm) == len(nums):
+                # CRITICAL: Append a COPY, otherwise res will be full of empty lists
+                res.append(perm[:]) 
+                return
+            
+            for i in range(len(nums)):
+                if not pick[i]:
+                    # Action: Choose the number
+                    perm.append(nums[i])
+                    pick[i] = True
+                    
+                    # Recurse
+                    backtrack(perm, pick)
+                    
+                    # Backtrack: Undo the choice
+                    pick[i] = False
+                    perm.pop()
+        
+        backtrack([], [False] * len(nums))
+        return res
