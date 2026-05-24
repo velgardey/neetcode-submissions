1d1dc6178815a85d62eb386fbbac853482fcbbdf
Mriganka Dey <arch.velgardey@gmail.com>
2026-05-24 13:30:34 +0530

    Add: lru-cache - submission-4

---
+++ Data Structures & Algorithms/lru-cache/submission-4.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    def insert(self, node):
+        prv, nxt = self.right.prev, self.right
+        prv.next, nxt.prev = node, node
+        node.next, node.prev = nxt, prv
+
+    def remove(self, node):
+        prv, nxt = node.prev, node.next
+        prv.next, nxt.prev = nxt, prv
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].val
+        return -1
+        
+
+    def put(self, key: int, val: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        self.cache[key] = Node(key, val)
+        self.insert(self.cache[key])
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
